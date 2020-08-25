d08a07a06c2c8efa80ac6a7a1a18af4442c08c52
Guillaume BOUDON <guillaumeboudon@gmail.com>
2020-08-25 17:04:51 +0200

    :bug: Add :proxy_url getter

--- lib/certeurope_sign_api.rb
+++ lib/certeurope_sign_api.rb
@@ -12,7 +12,7 @@ module CerteuropeSignAPI
   module Init
     require "openssl"
 
-    attr_reader :base_uri, :certificate, :key, :default_pdf_signature_options
+    attr_reader :base_uri, :certificate, :key, :default_pdf_signature_options, :proxy_url
 
     def init(base_uri:, pkcs12_path:, pkcs12_password:, proxy_url: nil)
       pkcs12 = OpenSSL::PKCS12.new(File.read(File.open(pkcs12_path)), pkcs12_password)
