f617f49eb9389d0f60903645515a29995048778f
Guillaume BOUDON <guillaumeboudon@gmail.com>
2020-11-13 09:19:21 +0100

    :recycle: Refactor code

--- test/support/helper.rb
+++ test/support/helper.rb
@@ -4,10 +4,11 @@ Dotenv.load(File.join(__dir__, ".env"))
 
 def init_certeurope_sign_api
   CerteuropeSignAPI.init(
-    base_uri:        ENV["CERTEUROPE_SIGN_API_URL"],
-    pkcs12_path:     File.join(__dir__, "certificate.p12"),
-    pkcs12_password: ENV["CERTEUROPE_SIGN_API_PKCS12_PASSWORD"],
-    )
+    base_uri: ENV["CERTEUROPE_SIGN_API_URL"],
+    pkcs12_path: File.join(__dir__, "certificate.p12"),
+    pkcs12_password: ENV["CERTEUROPE_SIGN_API_PKCS12_PASSWORD"]
+  )
+
   CerteuropeSignAPI.set_default_pdf_signature_options(
     {
       width: 250,
@@ -15,13 +16,12 @@ def init_certeurope_sign_api
       x: 50,
       y: 200,
       image_path: File.join(__dir__, "signature_template.png"),
-      lines_default_options:
-        {
-          line_color: 0,
-          line_font_size: 8,
-          line_font: "Times_roman",
-          line_pos_x_in_image: 45
-        }
+      lines_default_options: {
+        line_color: 0,
+        line_font_size: 8,
+        line_font: "Times_roman",
+        line_pos_x_in_image: 45
+      }
     }
   )
 end
